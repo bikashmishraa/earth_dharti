7309e8f36121b06675be9450252b351971b3ec2a
Sijal Manandhar <sijal.m06@gmail.com>
2024-10-06 15:30:09 +0545

    fixed importing in .html pages error

--- features/home.js
+++ features/home.js
@@ -2,11 +2,3 @@ const HomePage = document.getElementById("Home");
 HomePage.addEventListener("click", () => {
   window.location.href = "/";
 });
-
-const dataToggleButton = document.getElementById("ast_data_visual");
-const astroidDataContainer = document.getElementById("astroid-data-container");
-dataToggleButton.addEventListener("click", () => {
-  if (astroidDataContainer.style.display === "none")
-    astroidDataContainer.style.display = "block";
-  else astroidDataContainer.style.display = "none";
-});
